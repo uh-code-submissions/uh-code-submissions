7d1c6bd0e2b5091fa4ec13ddc5f255c93306474f
KanaiGooding <kgooding@hawaii.edu>
2021-04-15 12:01:22 -1000

    Background Color Changes

--- app/imports/startup/client/Startup.jsx
+++ app/imports/startup/client/Startup.jsx
@@ -9,3 +9,6 @@ import App from '../../ui/layouts/App.jsx';
 Meteor.startup(() => {
   render(<App />, document.getElementById('root'));
 });
+
+Meteor.startup(() =>  document.body.classList.add('uh-code-submissions-background'));
+
