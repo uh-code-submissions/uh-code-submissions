c083da6752c5449777cfb3a503c79e61d8c64e5c
kirandatwani <kiran25@hawaii.edu>
2021-04-14 21:04:55 -1000

    Landing page edited

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Header, Icon, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
+      <div className="code-landing-background" >
+        <Grid divided='vertically' id='landing-page' verticalAlign='middle' textAlign='center' container>
+          <Grid.Row columns={2}>
+            <Grid.Column width={10}>
+              <Image src="https://res.cloudinary.com/practicaldev/image/fetch/s--_kKDUKi9--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/j5axzkjkrhfvicea7uin.jpg"/>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              <Header as='h1' inverted>Welcome to UH Code Submissions!</Header>
+              <Header as='h4' inverted>A platform to prepare for technical interviews with challenging problems and quick feedback.</Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={3}>
+            <Grid.Column textAlign='center'>
+              <Icon size="huge" name="search" inverted></Icon>
+              <Header as='h2' inverted>Search for Problems</Header>
+              <Header as='h3' inverted></Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon size="huge" name="pencil" inverted></Icon>
+              <Header as='h2' inverted>Write Solutions</Header>
+              <Header as='h3' inverted></Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Icon size="huge" name="file alternate" inverted></Icon>
+              <Header as='h2' inverted>Get Immediate Feedback</Header>
+              <Header as='h3' inverted></Header>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
+      </div>
+
     );
   }
 }
