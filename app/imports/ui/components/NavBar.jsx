782c2a4edee7b0285c542a2765200b7e0716d201
KanaiGooding <kgooding@hawaii.edu>
2021-04-27 04:44:22 -1000

    Landing Pages changes and style updates

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,7 +11,7 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-      <Menu style={menuStyle} attached="top" borderless inverted color='black'>
+      <Menu style={menuStyle} attached="top" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <Header inverted as='h1'>UH Code Submissions</Header>
         </Menu.Item>
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,33 +5,30 @@ import { Grid, Header, Icon, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-      <div className="code-landing-background" >
+      <div className="code-background" >
         <Grid divided='vertically' id='landing-page' verticalAlign='middle' textAlign='center' container>
           <Grid.Row columns={2}>
             <Grid.Column width={10}>
             </Grid.Column>
             <Grid.Column width={8}>
-              <Header as='h1' inverted>Welcome to UH Code Submissions!</Header>
+              <h1 className="main-title">Welcome to UH Code Submissions!</h1>
               <Header as='h4' inverted>A platform to prepare for technical interviews with challenging problems and quick feedback.</Header>
               <Header as='h4' inverted>In conjunction with PANDA, Programing and Algorithms Club!</Header>
-              <Image src='../../../public/images/Panda.png' className="ui small image" centered />
             </Grid.Column>
           </Grid.Row>
+          <Image src='https://raw.githubusercontent.com/uh-code-submissions/uh-code-submissions/main/app/public/images/Panda.png' className="ui large image" centered />
           <Grid.Row columns={3}>
             <Grid.Column textAlign='center'>
               <Icon size="huge" name="search" inverted/>
               <Header as='h2' inverted>Search for Problems</Header>
-              <Header as='h3' inverted/>
             </Grid.Column>
             <Grid.Column textAlign='center'>
               <Icon size="huge" name="pencil" inverted/>
               <Header as='h2' inverted>Write Solutions</Header>
-              <Header as='h3' inverted/>
             </Grid.Column>
             <Grid.Column textAlign='center'>
               <Icon size="huge" name="file alternate" inverted/>
               <Header as='h2' inverted>Get Immediate Feedback</Header>
-              <Header as='h3' inverted/>
             </Grid.Column>
           </Grid.Row>
         </Grid>
