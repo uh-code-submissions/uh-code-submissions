812017dadeb0357d7de8deb9a71f506224b4b038
tristn <tristn@users.noreply.github.com>
2021-04-27 00:16:06 -1000

    Contacts

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Problems } from '../../api/problem/Problems';
 import { Notes } from '../../api/note/Notes';
+import { Contacts } from '../../api/contact/Contacts';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
@@ -17,6 +18,14 @@ Meteor.publish(Notes.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Notes.collection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+Meteor.publish(Contacts.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Contacts.collection.find({ owner: username });
   }
   return this.ready();
 });
