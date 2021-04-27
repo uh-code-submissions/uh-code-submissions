f25767906b46a72d99f721d646b9649f6ffca2c0
KanaiGooding <kgooding@hawaii.edu>
2021-04-27 00:51:24 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -9,28 +9,29 @@ class Landing extends React.Component {
         <Grid divided='vertically' id='landing-page' verticalAlign='middle' textAlign='center' container>
           <Grid.Row columns={2}>
             <Grid.Column width={10}>
-              <Image src="https://res.cloudinary.com/practicaldev/image/fetch/s--_kKDUKi9--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/j5axzkjkrhfvicea7uin.jpg"/>
             </Grid.Column>
             <Grid.Column width={8}>
               <Header as='h1' inverted>Welcome to UH Code Submissions!</Header>
               <Header as='h4' inverted>A platform to prepare for technical interviews with challenging problems and quick feedback.</Header>
+              <Header as='h4' inverted>In conjunction with PANDA, Programing and Algorithms Club!</Header>
+              <Image src='../../../public/images/Panda.png' className="ui small image" centered />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row columns={3}>
             <Grid.Column textAlign='center'>
-              <Icon size="huge" name="search" inverted></Icon>
+              <Icon size="huge" name="search" inverted/>
               <Header as='h2' inverted>Search for Problems</Header>
-              <Header as='h3' inverted></Header>
+              <Header as='h3' inverted/>
             </Grid.Column>
             <Grid.Column textAlign='center'>
-              <Icon size="huge" name="pencil" inverted></Icon>
+              <Icon size="huge" name="pencil" inverted/>
               <Header as='h2' inverted>Write Solutions</Header>
-              <Header as='h3' inverted></Header>
+              <Header as='h3' inverted/>
             </Grid.Column>
             <Grid.Column textAlign='center'>
-              <Icon size="huge" name="file alternate" inverted></Icon>
+              <Icon size="huge" name="file alternate" inverted/>
               <Header as='h2' inverted>Get Immediate Feedback</Header>
-              <Header as='h3' inverted></Header>
+              <Header as='h3' inverted/>
             </Grid.Column>
           </Grid.Row>
         </Grid>
